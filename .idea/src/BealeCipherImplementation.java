ee0928767c5691b3e470fa3c847e114555081b0d
kaltrina4 <kaltrine.heta@student.uni-pr.edu>
2025-04-20 00:55:15 +0200

    Shtimi i metodes generateBookFile

--- .idea/src/BealeCipherImplementation.java
+++ .idea/src/BealeCipherImplementation.java
@@ -5,4 +5,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BealeCipherImplementation {
+    public static void generateBookFile(String fileName) {
+        try{
+            FileWriter fw = new FileWriter(fileName);
+            fw.write("apple banana cat dog elephant fox grape house igloo jar kite lion moon nose owl panda queen rose sun tiger umbrella violin whale xylophone yellow zebra");
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
