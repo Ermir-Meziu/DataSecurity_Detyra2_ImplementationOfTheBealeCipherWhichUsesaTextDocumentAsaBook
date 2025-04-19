078ec863dfe9fa5c86009cdf32040d83a50c1ebb
Ermir Meziu <ermir.meziu@student.uni-pr.edu>
2025-04-20 01:40:53 +0200

    Creating main class and starting a interactive program

---
+++ .idea/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+class Interaktiv{
+    public static void menu()
+    {
+        System.out.println("1.Enkritpim.");
+        System.out.println("2.Dekriptim.");
+        System.out.println("3.Zgjedh nje mesazh te ri.");
+        System.out.println("4.Ndalo programin");
+        System.out.println();
+    }
+}
