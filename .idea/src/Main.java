5cc401d2b204b899b89a6b0a117c0ddc7dba21d3
Ermir Meziu <ermir.meziu@student.uni-pr.edu>
2025-04-20 13:38:28 +0200

    Finishing the interactive class

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
     }
 }
 class Interaktiv{
+    public static void menyra(String message, ArrayList<String> bookWords){
+        Scanner scanner = new Scanner(System.in);
+        String encryptedMessage ="";
+        while (true){
+            menu();
+            System.out.print("Zgjedhja juaj:");
+            int choice = scanner.nextInt();
+            System.out.println();
+            scanner.nextLine();
+
+            if (choice ==1){
+                System.out.println("Enkriptimi");
+                System.out.println("-----------");
+                encryptedMessage = Encryption.encrypt(message, bookWords);
+                System.out.println("Encrypted Message: " + encryptedMessage);
+                System.out.println(" ");
+            }
+            else if(choice == 2){
+                System.out.println("Dekriptimi");
+                System.out.println("------------");
+                String decryptedMessage=Decryption.decrypy(encryptedMessage,bookWords);
+                System.out.println("Decrypted Message: " + decryptedMessage);
+                System.out.println(" ");
+            }
+            else if(choice == 3){
+                System.out.print("Mesazhi: ");
+                message = scanner.nextLine();
+                System.out.println();
+            }
+            else if(choice == 4){
+                System.out.println("Programi ndaloi");
+                break;
+            }
+            else{
+                System.out.println("Perzgjedhja e gabuar, shiko menun dhe zgjedh perseri!!");
+            }
+        }
+    }
     public static void menu()
     {
         System.out.println("1.Enkritpim.");
